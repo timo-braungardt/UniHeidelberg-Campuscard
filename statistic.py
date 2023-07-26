1143421deff29d80c35bca9db8f9f2554453a4d6
Timo Braungardt <timo.braungardt@web.de>
2023-07-26 19:34:28 +0200

    segment statistic functions make statistics nicer

--- statistic.py
+++ statistic.py
@@ -1,39 +1,61 @@
 import pandas
 
 
-def calculateStatistics(table):
-    money_spent = 0
+def overallSpent(table):
+    sum = 0
     for value in table['Bezahlt']:
         if value < 0:
-            money_spent -= value
-    print(f"overall spent: {money_spent:.2f}€")
+            sum -= value
+    return sum
 
-    times_recharged = 0
+
+def timesRecharged(table):
+    count = 0
     for value in table['Status']:
         if value == "Aufgeladen":
-            times_recharged += 1
+            count += 1
+    return count
+
+
+def getSectors(table, sum_overall = 0):
+    table = table.drop(columns=['Datum/Zeit', 'Betrag', 'Privileg', 'Rabatt', 'Karte/ID', 'Status'])
+    # rename entries
+    table['Produkt'].replace({'Trocknen open price': 'wash',
+                         'Waschen': 'wash', 
+                         'Trocknen': 'wash', 
+                         'Kasse': 'buffet',
+                         'Mensa to Go': 'counter_food',
+                         'Essen Drehkreuz': 'counter_food',
+                         'A4 schwarz/weiss': 'print'}, inplace=True)
+    
+    result = pandas.DataFrame(columns=['service', 'times', 'paid'])
+    result.loc[len(result)] = ['wash',
+                               table.loc[table['Produkt'] == 'wash', 'Menge'].sum(),
+                               table.loc[table['Produkt'] == 'wash', 'Bezahlt'].sum()]
+    result.loc[len(result)] = ['buffet',
+                               table.loc[table['Produkt'] == 'buffet', 'Menge'].sum(),
+                               table.loc[table['Produkt'] == 'buffet', 'Bezahlt'].sum()]
+    result.loc[len(result)] = ['counter',
+                               table.loc[table['Produkt'] == 'counter_food', 'Menge'].sum(),
+                               table.loc[table['Produkt'] == 'counter_food', 'Bezahlt'].sum()]
+    result.loc[len(result)] = ['print',
+                               table.loc[table['Produkt'] == 'print', 'Menge'].sum(),
+                               table.loc[table['Produkt'] == 'print', 'Bezahlt'].sum()]
+    sum_rest = sum_overall + result['paid'].sum()
+    result.loc[len(result)] = ['other',
+                               -1,
+                               -sum_rest]
+    return result
+
+
+def calculateStatistics(table):
+    print("======== Statistics ========")
+    money_spent = overallSpent(table)
+    print(f"overall spent: {money_spent:.2f}€")
+
+    times_recharged = timesRecharged(table)
     print(f"times recharged: {times_recharged}")
-
-    products = dict()
-    for name, value in zip(table['Produkt'], table['Menge']):
-        if name in products:
-            products[name] += value
-        else:
-            products[name] = value
-    
-    times_washed = products['Trocknen open price'] if 'Trocknen open price' in products else 0
-    times_washed += products['Waschen'] if 'Waschen' in products else 0
-    times_washed += products['Trocknen'] if 'Trocknen' in products else 0
-
-    times_buffet = products['Kasse'] if 'Kasse' in products else 0
-    times_day = products['Mensa to Go'] if 'Mensa to Go' in products else 0
-    times_day += products['Essen Drehkreuz'] if 'Essen Drehkreuz' in products else 0
-
-    times_printed = products['A4 schwarz/weiss'] if 'A4 schwarz/weiss' in products else 0
-    
-    print(f"times washed: {times_washed}")
-    print(f"times buffet: {times_buffet}")
-    print(f"times daily dish: {times_day}")
-    print(f"pages printed: {times_printed}")
-
+    print('')
+    print(getSectors(table, money_spent).to_string(index=False))
+    print('')
     print(f"days used: {(table.iloc[0]['Datum/Zeit'] - table.iloc[-1]['Datum/Zeit']).days}")
